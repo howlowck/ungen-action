5ec54b6ed306adfc826021e94dd3a344b8d2d51c
Hao Luo <howlowck@gmail.com>
2023-02-27 09:27:13 -0600

    added ungen version conditional

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,13 @@ done
 
 echo "vars: $varArgs"
 
-ungen -i $1 -o $2 $keep $varArgs -zip
+if [ "$5" == "latest"]
+then
+    echo "Using ungen latest"
+    ungen -i $1 -o $2 $keep $varArgs -zip
+else
+    echo "Using version $5"
+    curl -o ungen.tar.gz -L https://github.com/howlowck/ungen/releases/download/$5/ungen_Linux_x86_64.tar.gz
+    tar -xvzf ungen.tar.gz
+    ./ungen -i $1 -o $2 $keep $varArgs -zip
+fi
